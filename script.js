d3ac98637684b4686194b68c9a6c4acb66cb451f
stephanie nunez <snunez1231@gmail.com>
2023-10-24 17:00:38 -0400

    added first prompt and added input validation using nan for the length

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword (){
   console.log ("you clicked the button")
 
-  return "Generated password!"
+  var lengthInput = prompt("How many characters would you like your password to contain?:");
+  var length = parseInt(lengthInput);
 
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return "";
+
+}
 }
 // Write password to the #password input
 function writePassword() {
@@ -19,3 +25,8 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+//Prompts:
+//1. password length (8 - 128 characters) 
+//2. include lowercase, uppercase, numeric,and/or special characters
+//3. validate the input. (At least one character type should be inputed)
+//4. display password 
