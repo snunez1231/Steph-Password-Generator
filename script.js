9071997d7386ee97ec9a1c8272ce5fda321a0e87
stephanie nunez <snunez1231@gmail.com>
2023-10-24 17:29:23 -0400

    defined the character type vaules and added if statments to define the allowed characters in order to generate password. Then added final mathrandom fuction to generate a random password based on the defined criteria.

--- script.js
+++ script.js
@@ -18,11 +18,45 @@ var includeUppercase= confirm("Include uppercase characters?")
 var includeNumeric= confirm("Include numeric characters?")
 var includeSpecial= confirm("Include special characters?")
 
+// Validates at least one character type is selected
 if (!(includeLowercase || includeUppercase || includeNumeric || includeSpecial)) {
     alert("Please select at least one character type.");
     return "";
 
 }
+  // Defines character sets based on criteria
+  var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numericChars = '0123456789';
+  var specialChars = '!@#$%^&*()_-+=<>?/';
+
+  var allowedChars = '';
+
+  if (includeLowercase) {
+    allowedChars += lowercaseChars;
+  }
+
+  if (includeUppercase) {
+    allowedChars += uppercaseChars;
+  }
+
+  if (includeNumeric) {
+    allowedChars += numericChars;
+  }
+  if (includeSpecial) {
+    allowedChars += specialChars;
+  }
+
+  // Generate password based on criteria
+  var password = '';
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
+
+  return password;
+
+
 }
 
 // Write password to the #password input
